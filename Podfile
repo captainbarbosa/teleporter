3660c6fa20aaf1f6a703483da50377c3abb4fe3d
Nadia Barbosa <nadiabarbosa@me.com>
2017-03-30 12:37:16 -0400

    Added geocoding

--- Podfile
+++ Podfile
@@ -1,5 +1,6 @@
 platform :ios, '8.0'
-
+use_frameworks!
 target 'teleporter' do
   pod 'Mapbox-iOS-SDK', '~> 3.4'
+  pod 'MapboxGeocoder.swift', '~> 0.6'
 end
--- teleporter/ViewController.swift
+++ teleporter/ViewController.swift
@@ -6,31 +6,55 @@
 //  Copyright © 2017 Nadia Barbosa. All rights reserved.
 //
 import Mapbox
+import MapboxGeocoder
 import UIKit
 
 class ViewController: UIViewController, MGLMapViewDelegate {
+    let geocoder = Geocoder.shared
 
     @IBOutlet var mapView: MGLMapView!
+    @IBOutlet weak var placeName: UILabel!
+    
+    var mapDidFinishLoading: Bool = false
+    
+    var optionalAnnotation: MGLPointAnnotation?
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     @IBAction func teleportToLocation(_ sender: UIButton) {
-        print("HI")
-    }
+        guard mapDidFinishLoading == true else { return }
+        
+        let annotation = MGLPointAnnotation()
+        annotation.coordinate = CLLocationCoordinate2D(latitude: 38.897435, longitude: -77.039679)
+        mapView.addAnnotation(annotation)
+        
+        let camera = mapView.camera
+        
+        camera.centerCoordinate = annotation.coordinate
+        camera.altitude = 20000
+        mapView.setCamera(camera, animated: true)
+        
+        let options = ReverseGeocodeOptions(coordinate: annotation.coordinate)
+        
+        _ = geocoder.geocode(options) { [weak self] (placemarks, attribution, error) in
+            guard let strongSelf = self else { return }
+            guard let placemark = placemarks?.first else {
+                return
+            }
+            
+            strongSelf.placeName.text = placemark.administrativeRegion?.name ?? "Not found"
+        }
 
-    //var center = CLLocationCoordinate2D(latitude: 38.897435, longitude: -77.039679)
+    }
+    
+    
+   
+
     
     func mapViewDidFinishLoadingMap(_ mapView: MGLMapView) {
-        
-//        func test() {
-//            let camera = MGLMapCamera(lookingAtCenter: center, fromDistance: 200000, pitch: 0, heading: 0)
-//            
-//            mapView.fly(to: camera, withDuration: 4,
-//                        peakAltitude: 3000, completionHandler: nil)
-//            
-//        }
+        mapDidFinishLoading = true
     }
     
     
