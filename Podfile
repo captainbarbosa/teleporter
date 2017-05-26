5ae46640b1b45fff4fb7eae10db8d9607377a17b
Nadia Barbosa <captainbarbosa@users.noreply.github.com>
2017-05-26 09:24:13 -0700

    Updates

--- Podfile
+++ Podfile
@@ -1,7 +1,7 @@
 platform :ios, ‘9.0’
 use_frameworks!
 target 'teleporter' do
-  pod 'Mapbox-iOS-SDK', '~> 3.4'
+  pod 'Mapbox-iOS-SDK', '~> 3.5'
   pod 'MapboxGeocoder.swift', '~> 0.6'
-  pod 'MapboxNavigation', '~> 0.3.0'
+  pod 'MapboxNavigation', '~> 0.3'
 end
--- teleporter/ViewController.swift
+++ teleporter/ViewController.swift
@@ -27,6 +27,8 @@ class ViewController: UIViewController, MGLMapViewDelegate {
         
         let recognizer = UILongPressGestureRecognizer(target: self, action: #selector(didLongPress(_:)))
         mapView.addGestureRecognizer(recognizer)
+        
+        mapView.showsUserLocation = true
     }
     
     // Set up a long press gesture reconizer that creates and annotation
@@ -40,23 +42,23 @@ class ViewController: UIViewController, MGLMapViewDelegate {
         
         let annotation = MGLPointAnnotation()
         annotation.coordinate = coordinate
+        annotation.title = "New location"
         
         mapView.addAnnotation(annotation)
         
-        //reverseGeocode(coordinate: annotation.coordinate)
+        // Reverse geocode the point at which the long press occurs
         reverseGeocode(coordinate: annotation.coordinate) { (postalAddress) in
             let formatter = CNPostalAddressFormatter()
             guard let postalAddress = postalAddress else {
                 self.placeName.text = "Not found"
-                //completion(nil)
                 return
             }
             
             self.placeName.text = formatter.string(from: postalAddress)
-    
         }
     }
     
+    // Clear annotations from the map view if button is pressed
     @IBAction func removeAnnotations(_ sender: UIButton) {
         guard mapDidFinishLoading == true else { return }
         
@@ -66,13 +68,70 @@ class ViewController: UIViewController, MGLMapViewDelegate {
     
     func mapViewDidFinishLoadingMap(_ mapView: MGLMapView) {
         mapDidFinishLoading = true
-        // call reverGeocode
     }
     
-    func test() {
-        
+    // Ensure annotations can show callouts
+    func mapView(_ mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
+        return true
     }
-    // Reverse geocode
+    
+    // Customize annotation callout
+    func mapView(_ mapView: MGLMapView, rightCalloutAccessoryViewFor annotation: MGLAnnotation) -> UIView? {
+        let bundle = Bundle(for: NavigationViewController.self)
+        let image = UIImage(named: "recenter", in: bundle, compatibleWith: nil)
+        let button = UIButton(type: .custom)
+        button.setImage(image, for: .normal)
+        button.sizeToFit()
+        return button
+    }
+    
+    // Delegate method for custom annotation callout
+    func mapView(_ mapView: MGLMapView, annotation: MGLAnnotation, calloutAccessoryControlTapped control: UIControl) {
+        
+        calculateRoute(from: mapView.userLocation!.coordinate, to: annotation.coordinate) { [unowned self] (route, error) in
+            //  Present navigation with route
+            if let error = error {
+              // Add alert view controller here
+            }
+            guard let route = route else {
+                return
+            }
+            self.presentNavigation(along: route)
+        }
+    }
+    
+    // Calculate route to be used for navigation
+    func calculateRoute(from origin: CLLocationCoordinate2D,
+                        to destination: CLLocationCoordinate2D,
+                        completion: @escaping (Route?, Error?) -> ()) {
+        // origin CLLocatuinCoordinate2D
+        let originWaypoint = Waypoint(coordinate: origin, name: "Mapbox")
+        // origin Waypoint
+        let destinationWaypoint = Waypoint(coordinate: destination)
+        
+        
+        let options = RouteOptions(waypoints: [originWaypoint, destinationWaypoint], profileIdentifier: .automobileAvoidingTraffic)
+        options.routeShapeResolution = .full
+        options.includesSteps = true
+        
+        _ = Directions.shared.calculate(options) { (waypoints, routes, error) in
+            
+            guard let route = routes?.first else {
+                completion(nil, error)
+                return
+            }
+            completion(route, error)
+        }
+    }
+    
+    // Present the navigation view controller
+    func presentNavigation(along route: Route) {
+      let viewController = NavigationViewController(for: route)
+      viewController.simulatesLocationUpdates = true
+      self.present(viewController, animated: true, completion: nil)
+    }
+    
+    // Reverse geocode sequence
     func reverseGeocode(coordinate: CLLocationCoordinate2D,
                         completion: @escaping (CNPostalAddress?) -> ()) {
         let options = ReverseGeocodeOptions(coordinate: coordinate) // 1 main thread
